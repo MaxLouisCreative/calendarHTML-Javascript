96fe4dfb0c32aa759ad24ccdc9f9b60b36bf8d78
Max <Maxlouis69@gmail.com>
2020-12-04 12:54:49 +0000

    Added functions for poening all modals

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,47 @@
-//var formdata;
+var formdata;
 
 $('.calendar-cell').on('click', function(){
 	var el = $(this).data();
 	console.log($(this).data());
-	openPopup(el);
+	createEvent(el);
 })
 
-function openPopup(el){
-	$(".popup-overlay").addClass("active");
+$('.grupa-event').on('click', function(){
+        var el = $(this).data();
+        console.log($(this).data());
+        editEvent(el);
+})
+
+$('.grupa-event_close').on('click', function(){
+        var el = $(this).data();
+        console.log($(this).data());
+        deleteEvent(el);
+})
+
+function createEvent(el){
+	$("#popup-create").addClass("active");
+        $(".container").addClass("background-overlay");
 	$("#selDay").val(el.day);
 	$("#selMonth").val(el.month);
 	$("#selYear").val(el.year);
+
+};
+
+function editEvent(el){
+        $("#popup-edit").addClass("active");
+        $(".container").addClass("background-overlay");
+        $("#selDay").val(el.day);
+        $("#selMonth").val(el.month);
+        $("#selYear").val(el.year);
+
+};
+
+function deleteEvent(el){
+        $("#popup-delete").addClass("active");
+        $(".container").addClass("background-overlay");
+        $("#selDay").val(el.day);
+        $("#selMonth").val(el.month);
+        $("#selYear").val(el.year);
 
 };
 
@@ -18,7 +49,7 @@ $("#grupa-form").submit(function(e){
         e.preventDefault();
         var form = $(this);
         var action = form.attr("action");
-        var formdata = form.serializeArray();
+        formdata = form.serializeArray();
         createJSON(formdata);
         // $.ajax({
         //             url: contextroot+action,
@@ -44,4 +75,5 @@ $(".popup-close").on("click", function(){
 
 function closePopup(){
 	$(".popup-overlay").removeClass("active");
+        $(".container").removeClass("background-overlay");
 }
