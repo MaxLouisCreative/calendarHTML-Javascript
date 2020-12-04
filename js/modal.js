a69ba77a90a23a387bf081524e2b34dc685b01d9
Nika Purkis <nika@purkis.ca>
2020-12-04 10:47:35 +0000

    onclick event for calnedar cell, populating hidden fields in popup

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,17 @@
 
-function openPopup(date){};
+$('.calendar-cell').on('click', function(){
+	var el = $(this).data();
+	console.log($(this).data());
+	openPopup(el);
+})
+
+function openPopup(el){
+	$(".popup-overlay").addClass("active");
+	$("#selDay").val(el.day);
+	$("#selMonth").val(el.month);
+	$("#selYear").val(el.year);
+
+};
 
 $(".grupa-today").on("click", function(){
 $(".popup, .popup-content").addClass("active");
